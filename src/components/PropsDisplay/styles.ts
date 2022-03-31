c7045d6a3b1a722d4341e2e73f575681b58552e6
kataik <erik.albuquerque.oficial@gmail>
2022-03-31 12:51:48 -0300

    fix: resize font in device mobile

--- src/components/PropsDisplay/styles.ts
+++ src/components/PropsDisplay/styles.ts
@@ -21,23 +21,25 @@ type Props = {
   isHover: boolean;
 };
 
-export const Props = styled.div<Props>`
-  background-color: ${(props) => (props.isCopy ? "#3db13d" : "var(--cyan)")};
-  padding: 0.5rem 1rem;
-  border-radius: 0.5rem;
-  user-select: none;
-  cursor: pointer;
-  transition: filter 0.2s, background-color 0.2s;
-  :hover {
-    filter: brightness(0.95);
-  }
-
-  span {
-    color: ${(props) => (props.isCopy ? "var(--white)" : "var(--brown-700)")};
-    font-weight: 700;
-    font-size: 1rem;
-  }
-`;
+export const Props = styled.div<Props>((props) => ({
+  backgroundColor: props.isCopy ? "#3db13d" : "var(--cyan)",
+  padding: "0.5rem 1rem",
+  borderRadius: "0.5rem",
+  userSelect: "none",
+  cursor: "pointer",
+  transition: "filter 0.2s, background-color 0.2s",
+  ":hover": {
+    filter: "brightness(0.95)",
+  },
+  span: {
+    color: props.isCopy ? "var(--white)" : "var(--brown-700)",
+    fontWeight: "700",
+    fontSize: "1rem",
+    [down("md")(props)]: {
+      fontSize: "0.875rem",
+    },
+  },
+}));
 
 export const Wrapper = styled.div`
   display: flex;
