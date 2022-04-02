0238be416d421aac1df96a70a5855f1845e44e51
kataik <erik.albuquerque.oficial@gmail>
2022-04-02 12:44:39 -0300

    feat: add responsive font size

--- src/styles/GlobalStyles.ts
+++ src/styles/GlobalStyles.ts
@@ -25,4 +25,16 @@ export const GlobalStyles = createGlobalStyle`
     font-family: 'Nunito', sans-serif;
     background-color: var(--white-50);
   }
+
+  @media (max-width: 1080px) {
+    html { 
+      font-size: 93.75%; // 15px
+    }
+  }
+
+  @media (max-width: 720px) {
+    html {
+      font-size: 87.5%; // 14px
+    }
+  }
 `;
